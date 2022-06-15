aac8d862bec5df20be3473989c895e5253bb7c63
Randy Bryant <Randy.Bryant@cs.cmu.edu>
2022-06-15 17:34:41 -0400

    Generate single clause CNF + CRAT

--- benchmarks/generators/writer.py
+++ benchmarks/generators/writer.py
@@ -1,4 +1,23 @@
-# Code for generating QCNF, orders, and clusters
+#####################################################################################
+# Copyright (c) 2021 Marijn Heule, Randal E. Bryant, Carnegie Mellon University
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+# associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute,
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+# OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+########################################################################################
+
+# Code for generating CNF, order, schedule, and crat proof files
 class WriterException(Exception):
 
     def __init__(self, value):
@@ -13,11 +32,17 @@ class Writer:
     outfile = None
     suffix = None
     verbose = False
-    variableCount = None
+    expectedVariableCount = None
+    isNull = False
+    froot = ""
 
-    def __init__(self, froot, suffix = None, verbose = False):
-        self.variableCount = 0
+    def __init__(self, count, froot, suffix = None, verbose = False, isNull = False):
+        self.expectedVariableCount = count
+        self.froot = froot
         self.verbose = verbose
+        self.isNull = isNull
+        if isNull:
+            return
         if suffix is not None:
             self.suffix = suffix 
             fname = froot if self.suffix is None else froot + "." + self.suffix
@@ -27,117 +52,276 @@ class Writer:
             print("Couldn't open file '%s'. Aborting" % fname)
             sys.exit(1)
 
-    def countVariables(self, count):
-        self.variableCount += count
-
     def trim(self, line):
         while len(line) > 0 and line[-1] == '\n':
             line = line[:-1]
         return line
 
+    def vcount(self):
+        return self.expectedVariableCount
+
     def show(self, line):
+        if self.isNull:
+            return
         line = self.trim(line)
+        if self.verbose:
+            print(line)
         if self.outfile is not None:
             self.outfile.write(line + '\n')
 
     def finish(self):
+        if self.isNull:
+            return
         if self.outfile is None:
             return
         self.outfile.close()
         self.outfile = None
 
 
+     
+
 # Creating CNF
-class QcnfWriter(Writer):
+class CnfWriter(Writer):
     clauseCount = 0
     outputList = []
-    # Quantification type for each level
-    quantifications = {}
-    # Mapping from level to list of variables
-    variableLists = {}
-    # List of comments for each level of variables
-    variableCommentLists = {}
 
-    def __init__(self, froot, verbose = False):
-        Writer.__init__(self, froot, suffix = "qcnf", verbose = verbose)
+    def __init__(self, count, froot, verbose = False):
+        Writer.__init__(self, count, froot, suffix = "cnf", verbose = verbose)
         self.clauseCount = 0
-        self.ouputList = []
-        self.quantifications = {}
-        self.variableLists = {}
-        self.varableCommentLists = {}
+        self.outputList = []
 
     # With CNF, must accumulate all of the clauses, since the file header
     # requires providing the number of clauses.
-    # Variable list 0 denotes free variables
-    def addVariables(self, level, vlist, isExistential):
-        self.countVariables(len(vlist))
-        if level == 0:
-            return
-        if level in self.quantifications:
-            if self.quantifications[level] != isExistential:
-                print("Attempting to add variables %s.  Quantification %s" % (str(vlist), "existential" if isExistential else "universal"))
-                print("Existing variables at level %d: %s.  Quantification %s" % (level, str(self.variableLists[level]), "existential" if self.quantifications[level] else "universal"))
-                raise WriterException("Attempt different quantifications at level %d" % level)
-            self.variableLists[level] += vlist
-        else:
-            self.quantifications[level] = isExistential
-            self.variableLists[level] = list(vlist)
 
-    def addVariable(self, level, var, isExistential):
-        self.countVariables(1)
-        if level in self.quantifications:
-            if self.quantifications[level] != isExistential:
-                print("Attempting to add variable %d.  Quantification %s" % (var, "existential" if isExistential else "universal"))
-                print("Existing variables at level %d: %s.  Quantification %s" % (level, str(self.variableLists[level]), "existential" if self.quantifications[level] else "universal"))
-                raise WriterException("Attempt different quantifications at level %d" % level)
-            self.variableLists[level].append(var)
-        else:
-            self.quantifications[level] = isExistential
-            self.variableLists[level] = [var]
-
-            
     def doComment(self, line):
         self.outputList.append("c " + line)
 
-    def doVariableComment(self, level, line):
-        if level not in self.variableCommentLists:
-            self.variableCommentLists[level] = []
-        self.variableCommentLists[level].append(line)
-
     def doClause(self, literals):
+        for lit in literals:
+            var = abs(lit)
+            if var <= 0 or var > self.expectedVariableCount:
+                raise WriterException("Variable %d out of range 1--%d" % (var, self.expectedVariableCount))
         ilist = literals + [0]
-        self.clauseCount += 1
         self.outputList.append(" ".join([str(i) for i in ilist]))
+        self.clauseCount += 1
+        return self.clauseCount
 
     def finish(self):
+        if self.isNull:
+            return
         if self.outfile is None:
             return
-        self.show("p cnf %d %d" % (self.variableCount, self.clauseCount))
-        levels = sorted(self.quantifications.keys())
-        if len(levels) > 0 and levels[0] == -1:
-            levels = levels[1:] + [-1]
-        for level in levels:
-            if level in self.variableCommentLists:
-                for line in self.variableCommentLists[level]:
-                    self.show("c " + line + '\n')
-            qchar = 'e' if self.quantifications[level] else 'a'
-            slist = [str(v) for v in self.variableLists[level]]
-            line = qchar + ' ' + ' '.join(slist) + ' 0'
-            self.show(line)
+        self.show("p cnf %d %d" % (self.expectedVariableCount, self.clauseCount))
         for line in self.outputList:
             self.show(line)
         self.outfile.close()
         self.outfile = None
+
+# Enable permuting of variables before emitting CNF
+class Permuter:
+    forwardMap = {}
+    reverseMap = {}
     
+    def __init__(self, valueList = [], permutedList = []):
+        self.forwardMap = {}
+        self.reverseMap = {}
+        identity = False
+        if len(permutedList) == 0:
+            permutedList = valueList
+            identity = True
+        if len(valueList) != len(permutedList):
+            raise WriterException("Unequal list lengths: %d, %d" % (len(valueList), len(permutedList)))
+        for v, p in zip(valueList, permutedList):
+            self.forwardMap[v] = p
+            self.reverseMap[p] = v
+        if identity:
+            return
+        # Check permutation
+        for v in valueList:
+            if v not in self.reverseMap:
+                raise WriterException("Not permutation: Nothing maps to %s" % str(v))
+        for v in permutedList:
+            if v not in self.forwardMap:
+                raise WriterException("Not permutation: %s does not map anything" % str(v))
+            
+            
+    def forward(self, v):
+        if v not in self.forwardMap:
+            raise WriterException("Value %s not in permutation" % (str(v)))
+        return self.forwardMap[v]
+
+    def reverse(self, v):
+        if v not in self.reverseMap:
+            raise WriterException("Value %s not in permutation range" % (str(v)))
+        return self.reverseMap[v]
+    
+    def __len__(self):
+        return len(self.forwardMap)
+
+
+# Creating CNF incrementally.  Don't know number of variables in advance
+class LazyCnfWriter:
+
+    variableCount = 0
+    # Set of tuples (T/F, item)
+    # Boolean T for clause F for comment
+    # item: list of literals for clause, string for comment
+    items = []
+    froot = ""
+    verbose = False
+    permuter = None
+
+    def __init__(self, froot, permuter = None, verbose = False):
+        self.variableCount = 0
+        self.items = []
+        self.froot = froot
+        self.permuter = permuter
+        self.verbose = verbose
+
+
+    def newVariable(self):
+        self.variableCount += 1
+        if self.permuter is not None:
+            return self.permuter.reverse(self.variableCount)
+        else:
+            return self.variableCount
+
+    def vcount(self):
+        return self.variableCount
+
+    def newVariables(self, n):
+        return [self.newVariable() for i in range(n)]
+    
+    def doComment(self, line):
+        self.items.append((False, line))
+
+    def doClause(self, lits):
+        self.items.append((True, lits))
+
+    def clauseList(self):
+        clist = []
+        for (isClause, value) in self.items:
+            if isClause:
+                clist.append(value)
+        return clist
+
+    def finish(self):
+        writer = CnfWriter(self.variableCount, self.froot, self.verbose)
+        for (isClause, value) in self.items:
+            if isClause:
+                writer.doClause(value)
+            else:
+                writer.doComment(value)
+        writer.finish()
+        print("c File '%s.cnf' has %d variables and %d clauses" % (self.froot, self.variableCount, writer.clauseCount))
+
+# Creating LRAT proof
+class LratWriter(Writer):
+
+    # Must initialize this to the number of clauses in the original CNF file
+    clauseCount = 0
+
+    def __init__(self, clauseCount, froot, verbose = False):
+        Writer.__init__(self, None, froot, suffix = "lrat", verbose = verbose)
+        self.clauseCount = clauseCount
+
+    def doComment(self, line):
+        self.show("c " + line)
+
+    def doStep(self, lits, ids):
+        self.clauseCount += 1
+        ilist = [self.clauseCount] + lits + [0] + ids + [0]
+        self.show(" ".join([str(i) for i in ilist]))
+        return self.clauseCount
+     
+    
+class ScheduleWriter(Writer):
+    # Track potential errors
+    stackDepth = 0
+    decrementAnd = False
+    expectedFinal = 1
+
+    def __init__(self, count, froot, verbose = False, isNull = False):
+        Writer.__init__(self, count, froot, suffix = "schedule", verbose = verbose, isNull = isNull)
+        self.stackDepth = 0
+        self.decrementAnd = False
+    
+    def getClauses(self, clist):
+        self.show("c %s" % " ".join([str(c) for c in clist]))
+        self.stackDepth += len(clist)
+
+    # First time with new tree, want one fewer and operations
+    def newTree(self):
+        self.decrementAnd = True
+
+    def doAnd(self, count):
+        if self.decrementAnd:
+            count -= 1
+        self.decrementAnd = False
+        if count == 0:
+            return
+        if count+1 > self.stackDepth:
+            print("Warning: Cannot perform %d And's.  Only %d elements on stack" % (count, self.stackDepth))
+        self.show("a %d" % count)
+        self.stackDepth -= count
+
+    def doStore(self, name):
+        self.show("s %s" % name)
+
+    def doRetrieve(self, name):
+        self.show("r %s" % name)
+
+    def doDelete(self, name):
+        self.show("d %s" % name)
+
+    def doEquality(self):
+        self.show("e")
+
+    def doQuantify(self, vlist):
+        if self.isNull:
+            return
+        if self.stackDepth == 0:
+            print ("Warning: Cannot quantify.  Stack empty")
+#            raise WriterException("Cannot quantify.  Stack empty")
+        self.show("q %s" % " ".join([str(c) for c in vlist]))
+
+    # Issue equation or constraint.
+    def doPseudoBoolean(self, vlist, clist, const, isEquation=True):
+        if self.isNull:
+            return
+        # Anticipate that shifting everything from CNF evaluation to pseudoboolean reasoning
+        self.expectedFinal = 0
+        if self.stackDepth == 0:
+            print ("Warning: Cannot quantify.  Stack empty")
+        if len(vlist) != len(clist):
+            raise WriterException("Invalid equation or constraint.  %d variables, %d coefficients" % (len(vlist), len(clist)))
+        cmd = "=" if isEquation else ">="
+        slist = [cmd, str(const)]
+        slist += [("%d.%d" % (c,v)) for (c,v) in zip(clist, vlist)]
+        self.show(" ".join(slist))
+        self.stackDepth -= 1
+
+    def doComment(self, cstring):
+        self.show("# " + cstring)
+
+    def doInformation(self, cstring):
+        self.show("i " + cstring)
+
+    def finish(self):
+        if self.isNull:
+            return
+        if self.stackDepth != self.expectedFinal:
+            print("Warning: Invalid schedule.  Finish with %d elements on stack" % self.stackDepth)
+#            raise WriterException("Invalid schedule.  Finish with %d elements on stack" % self.stackDepth)
+        Writer.finish(self)
 
 class OrderWriter(Writer):
     variableList = []
 
-    def __init__(self, count, froot, verbose = False, suffix = None):
+    def __init__(self, count, froot, verbose = False, suffix = None, isNull = False):
         if suffix is None:
             suffix = "order"
-        Writer.__init__(self, froot, suffix = suffix, verbose = verbose)
-        self.countVariables(count)
+        Writer.__init__(self, count, froot, suffix = suffix, verbose = verbose, isNull = isNull)
         self.variableList = []
 
     def doOrder(self, vlist):
@@ -145,43 +329,81 @@ class OrderWriter(Writer):
         self.variableList += vlist
 
     def finish(self):
-        if self.variableCount != len(self.variableList):
+        if self.isNull:
+            return
+        if self.expectedVariableCount != len(self.variableList):
 #            raise WriterException("Incorrect number of variables in ordering %d != %d" % (
-#                len(self.variableList), self.variableCount))
+#                len(self.variableList), self.expectedVariableCount))
             print("Warning: Incorrect number of variables in ordering")
-            print("  Expected %d.  Got %d" % (self.variableCount, len(self.variableList)))
+            print("  Expected %d.  Got %d" % (self.expectedVariableCount, len(self.variableList)))
 
-        expected = list(range(1, self.variableCount+1))
+        expected = range(1, self.expectedVariableCount+1)
         self.variableList.sort()
         for (e, a) in zip(expected, self.variableList):
             if e != a:
-               print("Warning: Mismatch in ordering.  Expected %d.  Got %d" % (e, a))
+               raise WriterException("Mismatch in ordering.  Expected %d.  Got %d" % (e, a))
+        print("c File '%s.order' written" % (self.froot))
         Writer.finish(self)
 
-class ClusterWriter(Writer):
 
-    def __init__(self, count, froot, verbose = False, suffix = None):
-        if suffix is None:
-            suffix = "cluster"
-        Writer.__init__(self, froot, suffix = suffix, verbose = verbose)
-        self.countVariables(count)
-        self.variableList = []
+class CratWriter(Writer):
+    variableCount = 0
+    stepCount = 0
 
-    def doCluster(self, vlist):
-        self.show(" ".join([str(c) for c in vlist]))        
-        self.variableList += vlist
+    def __init__(self, variableCount, clauseList, froot, verbose = False):
+        Writer.__init__(self, variableCount, froot, suffix="crat", verbose=verbose, isNull=False)
+        self.doComment("Input clauses")
+        for lits in clauseList:
+            self.doStep(['i'] + lits + [0])
+        self.variableCount = variableCount
 
+    def doStep(self, items):
+        self.stepCount += 1
+        slist = [str(self.stepCount)] + [str(i) for i in items]
+        self.show(" ".join(slist))
+        return self.stepCount
+
+    def doNoStep(self, items):
+        slist = [str(i) for i in items]
+        self.show(" " + " ".join(slist))
+
+    def doComment(self, line):
+        self.show("c " + line)
+        
+    def doAnd(self, i1, i2):
+        self.variableCount += 1
+        v = self.variableCount
+        s = self.doStep(['p', v, i1, i2, 0])
+        if self.verbose:
+            self.doComment("Implicit declarations:")
+            self.doComment("%d a %d %d %d 0" % (s, v, -i1, -i2))
+            self.doComment("%d a %d %d 0" % (s+1, -v, i1))
+            self.doComment("%d a %d %d 0" % (s+2, -v, i2))
+        self.stepCount += 2
+        return self.variableCount
+
+    def doOr(self, i1, i2, hints = ['*']):
+        self.variableCount += 1
+        v = self.variableCount
+        s = self.doStep(['s', v, i1, i2] + hints +  [0])
+        if self.verbose:
+            self.doComment("Implicit declarations:")
+            self.doComment("%d a %d %d %d 0" % (s, -v, i1, i2))
+            self.doComment("%d a %d %d 0" % (s+1, v, -i1))
+            self.doComment("%d a %d %d 0" % (s+2, v, -i2))
+        self.stepCount += 2
+        return self.variableCount
+        
+    def doClause(self, lits, hints = ['*']):
+        self.doStep(['a'] + lits + [0] + hints + [0])
+        
+    def doDeleteClause(self, id, hints=['*']):
+        self.doNoStep(['dc', id] + hints + [0])
+
+    def doDeleteOperation(self):
+        self.doNoStep(['do', id])
+        
     def finish(self):
-        if self.variableCount != len(self.variableList):
-#            raise WriterException("Incorrect number of variables in ordering %d != %d" % (
-#                len(self.variableList), self.variableCount))
-            print("Warning: Incorrect number of variables in cluster")
-            print("  Expected %d.  Got %d" % (self.variableCount, len(self.variableList)))
+        print("c File '%s.crat' has %d variables and %d steps" % (self.froot, self.variableCount, self.stepCount))
+        Writer.finish(self)
 
-        expected = range(1, self.variableCount+1)
-        self.variableList.sort()
-        for (e, a) in zip(expected, self.variableList):
-            if e != a:
-               raise WriterException("Mismatch in clustered variables.  Expected %d.  Got %d" % (e, a))
-        self.writer.finish(self)
-    
