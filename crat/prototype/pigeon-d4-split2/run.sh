6719552fa25507133a13eb65efe7a7f20bb79711
Marienus Heule <mheule@SCS003B8.SP.CS.CMU.EDU>
2022-09-30 09:29:10 -0400

    run.sh

---
+++ crat/prototype/pigeon-d4-split2/run.sh
@@ -0,0 +1,10 @@
+BASE=$1
+
+cat $BASE.drat | grep -v "d " > $BASE.rat
+./negate $BASE.rat > $BASE.cubes
+cat $BASE.acnf $BASE.cubes | awk '/p cnf/ {print "p inccnf"} / 0/ {print $0}'> $BASE.icnf
+~/cadical/build/cadical $BASE.icnf $BASE.drup --no-binary
+cat $BASE.drup | grep -v "d " > $BASE.rup
+../crat-lrat/crat-lrat $BASE.acnf $BASE.rup -f | grep -v "c " | grep " 0" > result.txt
+cat result.txt
+
