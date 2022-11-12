503568995b83ca01c259a3b3ce3046195b070e02
Randal Bryant <Randy.Bryant@cs.cmu.edu>
2022-11-11 21:23:33 -0500

    Work on bug fix

--- crat/prototype/tdpog.py
+++ crat/prototype/tdpog.py
@@ -105,12 +105,12 @@ class Reasoner:
         return cid
 
     # Find clause that is subset of target
-    def findClauseId(self, tclause, maxCategory):
+    def findClauseId(self, tclause, maxCategory, equal = False):
         if mapInputClauseSetting:
             tclause = readwrite.cleanClause(tclause)
             if tclause in self.inputClauseMap:
                 return self.inputClauseMap[tclause]
-            if maxCategory == 0:
+            if maxCategory == 0 and equal:
                 return -1
         maxCid = self.getMaxStep(maxCategory)
         for (cat,cid,clause) in self.stepList:
@@ -928,7 +928,7 @@ class Pog:
         if self.hintLevel >= 2:
             # See if can find matching input clause
             tclause = [lit] + readwrite.invertClause(context)
-            cid = self.reasoner.findClauseId(tclause, maxCategorySetting)
+            cid = self.reasoner.findClauseId(tclause, maxCategorySetting, equal = True)
             if cid > 0:
                 if self.verbLevel >= 3:
                     print("Found input/argument clause #%d=%s justifying unit literal %d in context %s.  Adding as hint" % (cid, self.reasoner.getClause(cid), lit, str(context)))
