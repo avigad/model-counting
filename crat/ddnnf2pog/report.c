f8da977ca77d60e3da61adde67c433b3aef09e95
Randy Bryant <Randy.Bryant@cs.cmu.edu>
2023-01-07 10:07:10 -0500

    Add reporting routines

---
+++ crat/ddnnf2pog/report.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "report.h"
+
+int verblevel = 1;
+
+FILE *errfile = NULL;
+FILE *verbfile = NULL;
+
+void set_verblevel(int level) {
+    verblevel = level;
+}
+
+void err(bool fatal, const char *fmt, ...) {
+    if (!errfile)
+	errfile = stdout;
+    va_list ap;
+    va_start(ap, fmt);
+    fprintf(errfile, "ERROR: ");
+    vfprintf(errfile, fmt, ap);
+    fflush(errfile);
+    va_end(ap);
+    if (fatal)
+	exit(1);
+}
+
+void report(int level, const char *fmt, ...) {
+    if (!verbfile)
+	verbfile = stdout;
+    va_list ap;
+    if (level <= verblevel) {
+	va_start(ap, fmt);
+	vfprintf(verbfile, fmt, ap);
+	fflush(verbfile);
+	va_end(ap);
+    }
+}
+
---
+++ crat/ddnnf2pog/report.h
@@ -0,0 +1,70 @@
+/*========================================================================
+  Copyright (c) 2023 Randal E. Bryant, Carnegie Mellon University
+  
+  Permission is hereby granted, free of
+  charge, to any person obtaining a copy of this software and
+  associated documentation files (the "Software"), to deal in the
+  Software without restriction, including without limitation the
+  rights to use, copy, modify, merge, publish, distribute, sublicense,
+  and/or sell copies of the Software, and to permit persons to whom
+  the Software is furnished to do so, subject to the following
+  conditions:
+  
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+========================================================================*/
+
+
+#pragma once
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdbool.h>
+
+/* Default reporting level.  Must recompile when change */
+#ifndef RPT
+#define RPT 2
+#endif
+
+/* Ways to report interesting behavior and errors */
+
+/* Buffer sizes */
+#define MAX_CHAR 512
+
+/* Allow this headerfile to define C++ constructs if requested */
+#ifdef __cplusplus
+#define CPLUSPLUS
+#endif
+
+#ifdef CPLUSPLUS
+extern "C" {
+#endif
+
+extern int verblevel;
+void set_verblevel(int level);
+
+extern FILE *errfile;
+extern FILE *verbfile;
+
+/* Report Errors */
+void err(bool fatal, const char *fmt, ...);
+/* Report useful information */
+void report(int verblevel, const char *fmt, ...);
+
+
+#ifdef CPLUSPLUS
+}
+#endif
+
+
+/* EOF */
+
