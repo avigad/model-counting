3c2c9299b1d3af2c845af6bafebade9ee1f5361b
Jeremy Avigad <avigad@cmu.edu>
2023-01-22 07:51:26 -0500

    Rename to ItegCount and finish

--- lean4/ItegCount/ItegCount/Parse.lean
+++ lean4/ItegCount/ItegCount/Parse.lean
@@ -1,23 +1,23 @@
-import ModelCount.Iteg
+import ItegCount.Iteg
 
 namespace String
 
-def readNum (s : Substring) : Nat × Substring := do
+def readNum (s : Substring) : Nat × Substring := Id.run do
   let s₀ := s.dropWhile (fun c => ! c.isDigit)
   let s₁ := s₀.takeWhile Char.isDigit
-  let s₂ : Substring := ⟨s₀.1, s₁.3, s₀.3⟩ 
+  let s₂ : Substring := ⟨s₀.1, s₁.3, s₀.3⟩
   return (s₁.toNat?.get!, s₂)
 
 end String
 
 open Iteg
 
-def parseIte (s : String) : IteElt := do
-  let (n₀, s₀) := String.readNum s.toSubstring
+def parseIte (s : String) : IteElt := Id.run do
+  let (_, s₀) := String.readNum s.toSubstring
   let (n₁, s₁) := String.readNum s₀
   let (n₂, s₂) := String.readNum s₁
-  let (n₃, s₃) := String.readNum s₂
-  return IteElt.Ite n₁ n₂ n₃
+  let (n₃, _) := String.readNum s₂
+  return IteElt.ite n₁ n₂ n₃
 
 partial def readItegHeader (h : IO.FS.Handle) : IO (Nat × Nat × Nat × Nat) := do
   let s ← h.getLine
@@ -25,7 +25,7 @@ partial def readItegHeader (h : IO.FS.Handle) : IO (Nat × Nat × Nat × Nat) :=
     let (n₀, s₀) := String.readNum s.toSubstring
     let (n₁, s₁) := String.readNum s₀
     let (n₂, s₂) := String.readNum s₁
-    let (n₃, s₃) := String.readNum s₂
+    let (n₃, _) := String.readNum s₂
     return (n₀, n₁, n₂, n₃)
   else
     readItegHeader h
@@ -35,7 +35,7 @@ partial def readOutputDeclarations (h : IO.FS.Handle) : IO Nat := do
   let s ← h.getLine
   if "c Output".isPrefixOf s then
     let s ← h.getLine
-    let (n₀, s₀) := String.readNum s.toSubstring
+    let (n₀, _) := String.readNum s.toSubstring
     return n₀
   else
     readOutputDeclarations h
@@ -49,17 +49,17 @@ partial def findItes (h : IO.FS.Handle) : IO Unit := do
 
 def readIteg (fname : String) : IO (Nat × Nat × Nat × Array IteElt) := do
   let h ← IO.FS.Handle.mk fname IO.FS.Mode.read true
-  let (maxIndex, numInputs, numOutputs, numIteElts) ← readItegHeader h
+  let (_, numInputs, _, numIteElts) ← readItegHeader h
   let output ← readOutputDeclarations h
   let mut I : Array IteElt := #[]
-  I := I.push IteElt.Fls
-  I := I.push IteElt.Tr
+  I := I.push IteElt.fls
+  I := I.push IteElt.tr
   for i in [2:numInputs+2] do
-    I := I.push (IteElt.Var i)
+    I := I.push (IteElt.var i)
   findItes h
-  for i in [:numIteElts-numInputs] do
+  for _ in [:numIteElts] do
     I := (dbgTraceIfShared "not shared!" I).push $ parseIte (← h.getLine)
   return (numInputs, output, numIteElts, I)
-  
+
 
 
