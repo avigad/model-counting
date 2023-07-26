4f301a61c4233cc719ea52fe1f509d95e9f00390
Randy Bryant <Randy.Bryant@cs.cmu.edu>
2023-07-26 07:31:03 -0400

    Added ratio capability

--- tools/merge_csv.py
+++ tools/merge_csv.py
@@ -16,9 +16,10 @@ def eprint(s):
 
 
 def usage(name):
-    eprint("Usage: %s [-h] [-f]  [-s] [-l L0,L1,L2,...] FILE1.csv FILE2.csv ... FILEn.csv" % name)
+    eprint("Usage: %s [-h] [-f]  [-s I1:I2:...:Ik] [-r I1:I2] [-l L0,L1,L2,...] FILE1.csv FILE2.csv ... FILEn.csv" % name)
     eprint("  -f            Filter out lines that have at least one field missing")
     eprint("  -s I1:I2:...:Ik Sum the values from specified files 1..n and add as new column")
+    eprint("  -r I1:I2      Form the ratio between the values from specified files 1 and 2 and add as new column")
     eprint("  -h            Print this message")
     eprint("  -l LABELS     Provide comma-separated set of heading labels")
     eprint("  FILE1.csv ... Source files")
@@ -117,13 +118,30 @@ def sumEntries(sumSet):
         fields.append(sval)
     globalCount += 1
 
-def build(lstring, flist, doFilter, sumSet):
+def divideEntries(ratioSet):
+    global globalEntries, globalCount
+    for k in globalEntries.keys():
+        fields = globalEntries[k]
+        sfields = [fields[i] for i in range(globalCount) if i+1 in ratioSet]
+        try:
+            nums = [float(field) if len(field) > 0 else 0.0 for field in sfields]
+        except:
+            print("Couldn't divide fields for line with key %s.  Dividing over %s" % (k, str(sfields)))
+            sys.exit(1)
+        rval = 1000 * 1000.0 if nums[1] == 0 else nums[0]/nums[1]
+        fields.append(rval)
+    globalCount += 1
+
+
+def build(lstring, flist, doFilter, sumSet, ratioSet):
     first = True
     for fname in flist:
         nextFile(fname, first, doFilter)
         first = False
     if sumSet is not None:
         sumEntries(sumSet)
+    if ratioSet is not None:
+        divideEntries(ratioSet)
     if len(lstring) > 0:
         print(lstring)
     for k in sorted(globalEntries.keys()):
@@ -133,21 +151,29 @@ def build(lstring, flist, doFilter, sumSet):
 def run(name, args):
     doFilter = False
     sumSet = None
+    ratioSet = None
     lstring = ""
-    optList, args = getopt.getopt(args, "hfs:l:")
+    optList, args = getopt.getopt(args, "hfs:r:l:")
     for (opt, val) in optList:
         if opt == '-h':
             usage(name)
             sys.exit(0)
         elif opt == '-f':
             doFilter = True
-        elif opt == '-s':
+        elif opt == '-s' or opt == '-r':
             fields = val.split(':')
             try:
                 ivals = [int(field) for field in fields]
-                sumSet = set(ivals)
-            except:
-                eprint("Couldn't extract column numbers from argument '%s'" % val)
+                if opt == '-s':
+                    sumSet = set(ivals)
+                else:
+                    ratioSet = set(ivals)
+                    if len(ratioSet) != 2:
+                        eprint("Ratio can only be between two elements")
+                        usage(name)
+                        sys.exit(1)
+            except Exception as ex:
+                eprint("Couldn't extract column numbers from argument '%s' (%s)" % (val % str(ex)))
                 usage(name)
                 sys.exit(1)
         elif opt == '-l':
@@ -156,7 +182,7 @@ def run(name, args):
             eprint("Unknown option '%s'" % opt)
             usage(name)
             sys.exit(1)
-    build(lstring, args, doFilter, sumSet)
+    build(lstring, args, doFilter, sumSet, ratioSet)
 
 if __name__ == "__main__":
     run(sys.argv[0], sys.argv[1:])
