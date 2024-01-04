74271ca45d8f242bd361370bcc746c43db0c2478
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2024-01-04 01:39:29 +0100

    chore: add LeanSAT

--- lean4/ProofChecker/lakefile.lean
+++ lean4/ProofChecker/lakefile.lean
@@ -7,13 +7,10 @@ lean_lib ProofChecker
 
 require mathlib from git"https://github.com/leanprover-community/mathlib4"@"master"
 require Cli from git"https://github.com/mhuisi/lean4-cli"@"nightly"
+require LeanSAT from git"https://github.com/JamesGallicchio/LeanSAT"@"ppa-tauto"
 
 @[default_target]
 lean_exe checker {
   root := `Main
-  -- moreLeancArgs := #["-UNDEBUG", "-Og", "-ggdb", "-g3", "-fno-omit-frame-pointer"]
-  -- moreLinkArgs := #[
-  --   "-fuse-ld=/usr/bin/ld",
-  --   "-L/home/nawrocki/.elan/toolchains/leanprover--lean4---nightly-2022-10-31/lib"
-  -- ]
+  -- moreLeancArgs := #["-UNDEBUG", "-g", "-Og", "-ggdb", "-g3", "-fno-omit-frame-pointer"]
 }
